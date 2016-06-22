12892377d6abc0b7bfd3094a8e26db7fd957d170
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-06-22 17:14:44 +0900

    create the spec

--- spec/serverspec/default_spec.rb
+++ spec/serverspec/default_spec.rb
@@ -1,29 +1,23 @@
 require 'spec_helper'
 require 'serverspec'
 
-package = 'hadoop_journalnode'
-service = 'hadoop_journalnode'
+package = 'hadoop2'
+service = 'journalnode'
 config  = '/etc/hadoop_journalnode/hadoop_journalnode.conf'
-user    = 'hadoop_journalnode'
-group   = 'hadoop_journalnode'
-ports   = [ PORTS ]
-log_dir = '/var/log/hadoop_journalnode'
-db_dir  = '/var/lib/hadoop_journalnode'
+user    = 'hdfs'
+group   = 'hadoop'
+ports   = [ 8485, 8480 ]
+log_dir = '/var/log/hadoop'
+db_dir  = '/var/lib/journalnode'
 
 case os[:family]
 when 'freebsd'
-  config = '/usr/local/etc/hadoop_journalnode.conf'
-  db_dir = '/var/db/hadoop_journalnode'
+  db_dir = '/var/db/journalnode'
 end
 
 describe package(package) do
   it { should be_installed }
 end 
-
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape('hadoop_journalnode') }
-end
 
 describe file(log_dir) do
   it { should exist }
@@ -37,13 +31,6 @@ describe file(db_dir) do
   it { should be_mode 755 }
   it { should be_owned_by user }
   it { should be_grouped_into group }
-end
-
-case os[:family]
-when 'freebsd'
-  describe file('/etc/rc.conf.d/hadoop_journalnode') do
-    it { should be_file }
-  end
 end
 
 describe service(service) do
